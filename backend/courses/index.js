d553df4faad674f50413b2d5ed8928ed46c1ca1b
Ibtehaz Ali Chowdhury <ibtehaz.chowdhury@gmail.com>
2023-09-20 20:12:36 +1000

    updated Course Managemeent and Library to accept userIds

--- backend/courses/index.js
+++ backend/courses/index.js
@@ -12,10 +12,19 @@ mongoose
   .then((result) => console.log(result))
   .catch((err) => console.log(err));
 
-const CourseSchema = new mongoose.Schema({
+const courseSchema = new mongoose.Schema({
   name: String,
   capacity: Number,
-  enrolled: Number,
+  enrolledStudents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  faculty: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Course = mongoose.model("Course", CourseSchema);
@@ -27,29 +36,70 @@ app.get("/courses", async (req, res) => {
   res.send(courses);
 });
 
-app.post('/courses', async (req, res) => {
-    try {
-        const course = new Course(req.body);
-        await course.save();
-        res.status(201).send({ message: 'Course added', course });
-    } catch (error) {
-        res.status(500).send({ message: 'Error adding course', error: error.message });
-    }
+app.post("/courses", async (req, res) => {
+  try {
+    const course = new Course(req.body);
+    await course.save();
+    res.status(201).send({ message: "Course added", course });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Error adding course", error: error.message });
+  }
 });
 
-app.delete('/courses/:id', async (req, res) => {
-    try {
-        const course = await Course.findById(req.params.id);
-        if (!course) {
-            return res.status(404).send({ message: 'Course not found' });
-        }
-        await course.remove();
-        res.status(200).send({ message: 'Course deleted', course });
-    } catch (error) {
-        res.status(500).send({ message: 'Error deleting course', error: error.message });
+app.delete("/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).send({ message: "Course not found" });
     }
+    await course.remove();
+    res.status(200).send({ message: "Course deleted", course });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Error deleting course", error: error.message });
+  }
 });
 
+app.post("/courses/:courseId/enroll", async (req, res) => {
+  const { studentId } = req.body;
+
+  // First, verify the student exists in user-authentication service
+  const userResponse = await fetch(
+    `http://user-authentication-service-url/users/${studentId}`
+  );
+  const user = await userResponse.json();
+
+  if (user && user.role === "student") {
+    const course = await Course.findById(req.params.courseId);
+    course.enrolledStudents.push(studentId);
+    await course.save();
+    res.status(200).send({ message: "Student enrolled", course });
+  } else {
+    res.status(404).send({ message: "Student not found or not a student" });
+  }
+});
+
+app.post("/courses/:courseId/assign-faculty", async (req, res) => {
+  const { facultyId } = req.body;
+
+  // Verify the faculty exists in user-authentication service
+  const userResponse = await fetch(
+    `http://user-authentication-service-url/users/${facultyId}`
+  );
+  const user = await userResponse.json();
+
+  if (user && user.role === "faculty") {
+    const course = await Course.findById(req.params.courseId);
+    course.faculty = facultyId;
+    await course.save();
+    res.status(200).send({ message: "Faculty assigned", course });
+  } else {
+    res.status(404).send({ message: "Faculty not found or not a faculty" });
+  }
+});
 
 app.post("/enroll", async (req, res) => {
   const course = await Course.findById(req.body.courseId);
--- backend/library/index.js
+++ backend/library/index.js
@@ -1,7 +1,7 @@
 // /library-book-rental/index.js
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 mongoose
@@ -11,52 +11,88 @@ mongoose
   })
   .then((result) => console.log(result))
   .catch((err) => console.log(err));
-const BookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    availableCopies: Number,
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  rentedBy: {
+    type: mongoose.Schema.Types.ObjectId, // Pointing to the Student ID
+    ref: "User",
+    default: null, // When not rented, this will be null
+  },
 });
 
-const Book = mongoose.model('Book', BookSchema);
+const Book = mongoose.model("Book", BookSchema);
 
 app.use(express.json());
 
-app.get('/books', async (req, res) => {
-    const books = await Book.find({});
-    res.send(books);
+app.get("/books", async (req, res) => {
+  const books = await Book.find({});
+  res.send(books);
 });
 
-app.post('/books', async (req, res) => {
+app.post("/books", async (req, res) => {
   try {
-      const book = new Book(req.body);
-      await book.save();
-      res.status(201).send({ message: 'Book added', book });
+    const book = new Book(req.body);
+    await book.save();
+    res.status(201).send({ message: "Book added", book });
   } catch (error) {
-      res.status(500).send({ message: 'Error adding book', error: error.message });
+    res
+      .status(500)
+      .send({ message: "Error adding book", error: error.message });
   }
 });
 
-app.delete('/books/:id', async (req, res) => {
+app.delete("/books/:id", async (req, res) => {
   try {
-      const book = await Book.findById(req.params.id);
-      if (!book) {
-          return res.status(404).send({ message: 'Book not found' });
-      }
-      await book.remove();
-      res.status(200).send({ message: 'Book deleted', book });
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).send({ message: "Book not found" });
+    }
+    await book.remove();
+    res.status(200).send({ message: "Book deleted", book });
   } catch (error) {
-      res.status(500).send({ message: 'Error deleting book', error: error.message });
+    res
+      .status(500)
+      .send({ message: "Error deleting book", error: error.message });
   }
 });
 
+router.post("/rent", async (req, res) => {
+  const { bookId, studentId } = req.body;
 
-app.post('/rent', async (req, res) => {
-    const book = await Book.findById(req.body.bookId);
-    if (book && book.availableCopies > 0) {
-        book.availableCopies--;
-        await book.save();
-        res.send({ message: 'Book rented', book });
-    } else res.status(400).send({ message: 'Book unavailable' });
+  if (!bookId || !studentId) {
+    return res
+      .status(400)
+      .json({ error: "Book ID and Student ID are required." });
+  }
+
+  try {
+    const book = await Book.findById(bookId);
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found." });
+    }
+
+    if (book.rentedBy) {
+      return res.status(400).json({ error: "Book is already rented." });
+    }
+
+    book.rentedBy = studentId;
+    await book.save();
+
+    res.status(200).json({ message: "Book rented successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Server error." });
+  }
 });
 
-app.listen(3001, () => console.log('Library Book Rental Service running on port 3001'));
+app.listen(3001, () =>
+  console.log("Library Book Rental Service running on port 3001")
+);
